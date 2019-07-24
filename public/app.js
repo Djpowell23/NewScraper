709c904b7e55b47c0f7705ad37ffb2edd5faf7fa
Devin Powell <dpowel11@kent.edu>
2019-07-24 00:35:51 -0600

    note associations complete

--- public/app.js
+++ public/app.js
@@ -20,5 +20,48 @@ $(document).on("click", "#navClear", function () {
 
 $(document).on("click", "#addNote", function () {
     event.preventDefault();
-    console.log("this:", $(this));
+    console.log("this:", $(this).attr("data"));
+    var thisId = $(this).attr("data");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+        .then(function (data) {
+            console.log("ajax data:", data);
+            $("#notes").append("<h5>" + data.title.substring(0, 72) + '...' + "</h5>");
+            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<button class='btn-large blue waves-effect right' data-id='" + data._id + "' id='saveNote'>Save Note</button>");
+
+            // If there's a note in the article
+            if (data.note) {
+                $("#titleinput").val(data.note.title);
+                $("#bodyinput").val(data.note.body);
+            }
+        })
+});
+
+$(document).on("click", "#saveNote", function () {
+    event.preventDefault();
+
+    var thisId = $(this).attr("data-id");
+    console.log("ThisId:", thisId);
+
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    })
+        .then(function (data) {
+            console.log("last data:", data);
+            $("#notes").empty();
+        });
+
+    // Remove values entered
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
 });
